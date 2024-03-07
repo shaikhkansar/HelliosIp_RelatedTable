4aaf69af11b75b8779fae4cb2e23cde7690bde2e
Shaikh Ansar <ansar.shaikh@springuplabs.com>
2024-03-07 10:42:49 +0530

    Annuities Fetch Data Dynamically

--- src/components/Anuuities.jsx
+++ src/components/Anuuities.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./App.css";
 import Data from "./Data.json";
-import EditAnnuities from "./EditAnnuities"; // Import the EditAnnuities component
 import { Edit } from "react-feather";
 
 const Anuuities = ({ chatid }) => {
@@ -10,58 +9,100 @@ const Anuuities = ({ chatid }) => {
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
   const [editIndex, setEditIndex] = useState(null); // Track the index of the item being edited
+  const [editedFields, setEditedFields] = useState({});
+
+  const fetchData = async () => {
+    try {
+      const encodeddata = encodeURIComponent(chatid);
+      const modifiedEncodedData = encodeddata.replace(/%3A/g, "%3a");
+  
+      const response = await fetch(Data[1].url, {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          chatid: modifiedEncodedData,
+        }),
+      });
+  
+      if (!response.ok) {
+        throw new Error("Error fetching meeting details");
+      }
+  
+      const responseData = await response.json();
+  
+      if (!Array.isArray(responseData) || responseData.length === 0) {
+        throw new Error("Meeting details not found");
+      }
+  
+      setInstructionType(responseData);
+      setError(null);
+      setLoading(false);
+  
+      console.log("Meeting details:", responseData);
+    } catch (error) {
+      setError(`Error fetching meeting details: ${error.message}`);
+      setLoading(false);
+      console.error("Error fetching meeting details", error);
+    }
+  };
 
   useEffect(() => {
-    let isMounted = true;
-
-    const fetchData = async () => {
-      try {
-        const encodeddata = encodeURIComponent(chatid);
-        const modifiedEncodedData = encodeddata.replace(/%3A/g, "%3a");
-
-        const response = await fetch(Data[1].url, {
-          method: "POST",
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            chatid: modifiedEncodedData,
-          }),
-        });
-
-        if (!response.ok) {
-          throw new Error("Error fetching meeting details");
-        }
-
-        const data = await response.json();
-
-        if (isMounted) {
-          setInstructionType(data);
-          setError(null);
-          setLoading(false);
-        }
-
-        console.log("Meeting details:", data.value);
-      } catch (error) {
-        if (isMounted) {
-          setError(`Error fetching meeting details: ${error.message}`);
-          setLoading(false);
-        }
-        console.error("Error fetching meeting details", error);
-      }
-    };
-
     fetchData();
-
-    return () => {
-      isMounted = false;
-    };
   }, [chatid]);
 
   const handleEditClick = (index) => {
     setEditIndex(index);
   };
+
+  const handleInputChange = (e, key) => {
+    setEditedFields({
+      ...editedFields,
+      [key]: e.target.value,
+    });
+  };
+
+  const handleSaveChanges = async () => {
+    try {
+      // Get the hip_annuity value for the current editIndex
+      const hipAnnunity = instructionType[editIndex].hip_annuity;
+  
+      // Construct the payload with the edited fields and hip_annuity
+      const editedData = {
+        hip_annuity: "355e5ea6-a0fc-ec11-82e5-000d3a339091",
+        PayTerm: editedFields["PayTerm"] || instructionType[editIndex].PayTerm,
+        PayYear: editedFields["PayYear"] || instructionType[editIndex].PayYear,
+      };
+  
+      // Make a POST request to the endpoint with the payload
+      const response = await fetch("https://prod-143.westus.logic.azure.com:443/workflows/90a302fbee3e4bef8d2a0ff5954e6093/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=R_kKpbcUVfu9iiXwnMCwqu_yw8WzOEVz1GUJ1tfrpvY", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(editedData),
+      });
+  
+      // Check if the response is successful
+      if (!response.ok) {
+        throw new Error("Failed to save changes");
+      }
+  
+      // Log success message and reset state
+      console.log("Changes saved successfully");
+      setEditIndex(null);
+      setEditedFields({});
+  
+      // After saving changes, fetch meeting details again to update the state
+      fetchData();
+    } catch (error) {
+      console.error("Error saving changes:", error);
+    }
+  };
+  
+  
 
   return (
     <div className="App" style={{ marginTop: "40px", marginRight: "60px" }}>
@@ -103,16 +144,35 @@ const Anuuities = ({ chatid }) => {
                     <h3>Name: {instruction.Name}</h3>
                     <p>Primary Category: {instruction.PrimaryCategory}</p>
                     <p>Jurisdiction: {instruction.Jurisdiction}</p>
-                    <p>PayTerm: {instruction.PayTerm}</p>
-                    <p>PayYear: {instruction.PayYear}</p>
-                    {/* Add more properties as needed */}
-                    {editIndex === index ? ( // Render EditAnnuities component if index matches the editing index
-                    <EditAnnuities />
+                    <p>AnnuityID: {instruction.AnnuityID}</p>
+                    {editIndex === index ? (
+                      <>
+                        <label>PayTerm:</label>
+                        <input
+                          type="text"
+                          value={editedFields["PayTerm"] || instruction.PayTerm}
+                          onChange={(e) => handleInputChange(e, "PayTerm")}
+                        />
+                        <br />
+                        <label>PayYear:</label>
+                        <input
+                          type="text"
+                          value={editedFields["PayYear"] || instruction.PayYear}
+                          onChange={(e) => handleInputChange(e, "PayYear")}
+                        />
+                        <br />
+                        <button onClick={handleSaveChanges}>Save</button>
+                      </>
                     ) : (
-                      <Link className="link">
-                        <Edit onClick={() => handleEditClick(index)}/>
-                      </Link>
+                      <>
+                        <p>PayTerm: {instruction.PayTerm}</p>
+                        <p>PayYear: {instruction.PayYear}</p>
+                        {/* Add more properties as needed */}
+                      </>
                     )}
+                    <Link className="link">
+                      <Edit onClick={() => handleEditClick(index)} />
+                    </Link>
                   </div>
                 ))
               ) : (
--- src/components/EditAnnuities.jsx
+++ src/components/EditAnnuities.jsx
@@ -1,117 +1,117 @@
-import React, { useState,useEffect } from "react";
-import { Save, XSquare, Edit } from "react-feather";
+// import React, { useState,useEffect } from "react";
+// import { Save, XSquare, Edit } from "react-feather";
 
-const EditAnnuities = ({ initialPayTerm, initialPayYear }) => {
-  const [isEditing, setIsEditing] = useState(false);
-  const [payTerm, setPayTerm] = useState("");
-  const [payYear, setPayYear] = useState("");
+// const EditAnnuities = ({ initialPayTerm, initialPayYear }) => {
+//   const [isEditing, setIsEditing] = useState(false);
+//   const [payTerm, setPayTerm] = useState("");
+//   const [payYear, setPayYear] = useState("");
 
-  const handleEditClick = () => {
-    setIsEditing(true);
-  };
+//   const handleEditClick = () => {
+//     setIsEditing(true);
+//   };
 
-  // Update state when initial values change
-  useEffect(() => {
-    setPayTerm(initialPayTerm);
-    setPayYear(initialPayYear);
-  }, [initialPayTerm, initialPayYear]);
+//   // Update state when initial values change
+//   useEffect(() => {
+//     setPayTerm(initialPayTerm);
+//     setPayYear(initialPayYear);
+//   }, [initialPayTerm, initialPayYear]);
 
-  const handleSaveClick = () => {
-    setIsEditing(false);
-    // Format the payYear date to "dd/MM/yyyy" before sending
-    const formattedPayYear = formatDate(payYear);
+//   const handleSaveClick = () => {
+//     setIsEditing(false);
+//     // Format the payYear date to "dd/MM/yyyy" before sending
+//     const formattedPayYear = formatDate(payYear);
   
-    // Prepare data to send
-    const data = {
-      payTerm: payTerm,
-      payYear: formattedPayYear
-    };
+//     // Prepare data to send
+//     const data = {
+//       payTerm: payTerm,
+//       payYear: formattedPayYear
+//     };
   
-    // Send POST request to the endpoint
-    fetch('https://prod-143.westus.logic.azure.com:443/workflows/90a302fbee3e4bef8d2a0ff5954e6093/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=R_kKpbcUVfu9iiXwnMCwqu_yw8WzOEVz1GUJ1tfrpvY', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
-    })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Failed to save entry');
-      }
-      // Check if response contains any content
-      const contentType = response.headers.get('content-type');
-      if (contentType && contentType.includes('application/json')) {
-        // Parse JSON response
-        return response.json();
-      } else {
-        // No content in response, consider it a success
-        setIsEditing(false);
-        alert('Data saved successfully!');
-        return null; // Return null to skip further processing
-      }
-    })
-    .then(data => {
-      if (data !== null) {
-        // Handle successful response here if there's JSON data
-        console.log('Entry saved successfully:', data);
-      }
-    })
-    .catch(error => {
-      // Handle error
-      console.error('Error saving entry:', error);
-      // Optionally, you can display an error message to the user
-    });
-  };
+//     // Send POST request to the endpoint
+//     fetch('https://prod-143.westus.logic.azure.com:443/workflows/90a302fbee3e4bef8d2a0ff5954e6093/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=R_kKpbcUVfu9iiXwnMCwqu_yw8WzOEVz1GUJ1tfrpvY', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json'
+//       },
+//       body: JSON.stringify(data)
+//     })
+//     .then(response => {
+//       if (!response.ok) {
+//         throw new Error('Failed to save entry');
+//       }
+//       // Check if response contains any content
+//       const contentType = response.headers.get('content-type');
+//       if (contentType && contentType.includes('application/json')) {
+//         // Parse JSON response
+//         return response.json();
+//       } else {
+//         // No content in response, consider it a success
+//         setIsEditing(false);
+//         alert('Data saved successfully!');
+//         return null; // Return null to skip further processing
+//       }
+//     })
+//     .then(data => {
+//       if (data !== null) {
+//         // Handle successful response here if there's JSON data
+//         console.log('Entry saved successfully:', data);
+//       }
+//     })
+//     .catch(error => {
+//       // Handle error
+//       console.error('Error saving entry:', error);
+//       // Optionally, you can display an error message to the user
+//     });
+//   };
   
   
 
-  const handleCancelClick = () => {
-    // Handle canceling edit
-    setIsEditing(false);
-    // Reset input fields to initial values
-    setPayTerm(initialPayTerm);
-    setPayYear(initialPayYear);
-  };
+//   const handleCancelClick = () => {
+//     // Handle canceling edit
+//     setIsEditing(false);
+//     // Reset input fields to initial values
+//     setPayTerm(initialPayTerm);
+//     setPayYear(initialPayYear);
+//   };
 
-  const formatDate = (dateString) => {
-    const date = new Date(dateString);
-    const formattedDate = date.toLocaleDateString('en-GB', { day: '2-digit', month: '2-digit', year: 'numeric' });
-    return formattedDate;
-  };
+//   const formatDate = (dateString) => {
+//     const date = new Date(dateString);
+//     const formattedDate = date.toLocaleDateString('en-GB', { day: '2-digit', month: '2-digit', year: 'numeric' });
+//     return formattedDate;
+//   };
 
-  return (
-    <div>
-    {isEditing ? (
-      <div>
-        <label htmlFor="payTerm">Pay Term:</label>
-        <input
-          className="form-control"
-          type="text"
-          id="payTerm"
-          value={payTerm}
-          onChange={(e) => setPayTerm(e.target.value)}
-        />
-        <br />
-        <label htmlFor="payYear">Pay Year:</label>
-        <input
-          className="form-control"
-          type="Date"
-          id="payYear"
-          value={payYear}
-          onChange={(e) => setPayYear(e.target.value)}
-        />
-        <br />
-        <Save onClick={handleSaveClick} />
-        <XSquare onClick={handleCancelClick} />
-      </div>
-    ) : (
-      <div>
-        <Edit onClick={handleEditClick} />
-      </div>
-    )}
-  </div>
-  );
-};
+//   return (
+//     <div>
+//     {isEditing ? (
+//       <div>
+//         <label htmlFor="payTerm">Pay Term:</label>
+//         <input
+//           className="form-control"
+//           type="text"
+//           id="payTerm"
+//           value={payTerm}
+//           onChange={(e) => setPayTerm(e.target.value)}
+//         />
+//         <br />
+//         <label htmlFor="payYear">Pay Year:</label>
+//         <input
+//           className="form-control"
+//           type="Date"
+//           id="payYear"
+//           value={payYear}
+//           onChange={(e) => setPayYear(e.target.value)}
+//         />
+//         <br />
+//         <Save onClick={handleSaveClick} />
+//         <XSquare onClick={handleCancelClick} />
+//       </div>
+//     ) : (
+//       <div>
+//         <Edit onClick={handleEditClick} />
+//       </div>
+//     )}
+//   </div>
+//   );
+// };
 
-export default EditAnnuities;
+// export default EditAnnuities;
